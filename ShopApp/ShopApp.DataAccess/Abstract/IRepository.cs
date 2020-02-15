d9b6648a61f7dab7d0f760aa6c5e79cadb73e7dd
Okan <o.karadag@outlook.com>
2020-02-16 00:02:57 +0300

    Add repository interface

---
+++ ShopApp/ShopApp.DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace ShopApp.DataAccess.Abstract
+{
+    public interface IRepository<TEntity>
+    {
+        TEntity GetById(int id);
+        TEntity GetOne(Expression<Func<TEntity, bool>> filter);
+        IEnumerable <TEntity> GetAll(Expression<Func<TEntity, bool>> filter=null);
+        void Create();
+        void Update();
+        void Delete();
+    }
+}
