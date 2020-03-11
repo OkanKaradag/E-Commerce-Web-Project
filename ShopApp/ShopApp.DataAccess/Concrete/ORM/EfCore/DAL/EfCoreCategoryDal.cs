7821d3b75d01c4e907a318106c1607e7e6596a2e
Okan Karadağ <okan.karadag@d-teknoloji.com.tr>
2020-03-11 16:47:19 +0300

    Add EfCoreCategoryDal

---
+++ ShopApp/ShopApp.DataAccess/Concrete/ORM/EfCore/DAL/EfCoreCategoryDal.cs
@@ -0,0 +1,15 @@
+﻿using ShopApp.DataAccess.Abstract.DAL;
+using ShopApp.DataAccess.Concrete.EfCore.Repository;
+using ShopApp.Entities;
+using System.Collections.Generic;
+
+namespace ShopApp.DataAccess.Concrete.EfCore.DAL
+{
+    public class EfCoreCategoryDal : EfCoreRepository<Category, ShopContext>, ICategoryDal
+    {
+        public IEnumerable<Category> GetSubCategories(int categoryId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
