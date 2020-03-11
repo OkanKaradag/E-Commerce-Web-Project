2c283e92dc68958d57f0ce2ed2494da54b1cff57
Okan Karadağ <okan.karadag@d-teknoloji.com.tr>
2020-03-11 16:47:06 +0300

    Update EfCoreProductDal

--- ShopApp/ShopApp.DataAccess/Concrete/ORM/EfCore/DAL/EfCoreProductDal.cs
+++ ShopApp/ShopApp.DataAccess/Concrete/ORM/EfCore/DAL/EfCoreProductDal.cs
@@ -1,11 +1,11 @@
-﻿using ShopApp.DataAccess.Abstract;
+﻿using ShopApp.DataAccess.Abstract.DAL;
 using ShopApp.DataAccess.Concrete.EfCore.Repository;
 using ShopApp.Entities;
 using System.Collections.Generic;
 
 namespace ShopApp.DataAccess.Concrete.EfCore.DAL
 {
-    public class EfCoreProductDal : EfCoreRepository<Product, ShopContext>
+    public class EfCoreProductDal : EfCoreRepository<Product, ShopContext>, IProductDal
     {
         public IEnumerable<Product> GetPopularProducts()
         {
