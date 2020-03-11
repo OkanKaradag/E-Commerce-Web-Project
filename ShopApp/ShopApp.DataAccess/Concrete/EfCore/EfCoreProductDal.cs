be6bde37cf7d2f85227b5d92f272dd7f64fd857e
Okan Karadağ <okan.karadag@d-teknoloji.com.tr>
2020-03-11 15:06:24 +0300

    Add EfCoreProductDal

---
+++ ShopApp/ShopApp.DataAccess/Concrete/EfCore/EfCoreProductDal.cs
@@ -0,0 +1,14 @@
+﻿using ShopApp.DataAccess.Abstract;
+using ShopApp.Entities;
+using System.Collections.Generic;
+
+namespace ShopApp.DataAccess.Concrete.EfCore
+{
+    public class EfCoreProductDal : EfCoreRepository<Product, ShopContext>, IProductDal
+    {
+        public IEnumerable<Product> GetPopularProducts()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
