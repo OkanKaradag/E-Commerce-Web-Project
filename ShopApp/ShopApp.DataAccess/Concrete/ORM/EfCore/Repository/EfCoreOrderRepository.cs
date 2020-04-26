38fbc7189df1e6cea576782d3773bce6523428bb
Okan Karadağ <okan.karadag@d-teknoloji.com.tr>
2020-04-26 03:50:25 +0300

    Update EfCoreOrderDal

---
+++ ShopApp/ShopApp.DataAccess/Concrete/ORM/EfCore/Repository/EfCoreOrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.DataAccess.Abstract.DAL;
+using ShopApp.DataAccess.Concrete.EfCore.Repository;
+using ShopApp.Entities;
+using System;
+
+namespace ShopApp.DataAccess.Concrete.ORM.EfCore.DAL
+{
+    public class EfCoreOrderRepository : EfCoreRepository<Order>, IOrderRepository
+    {
+        private readonly DbContext _dbContext;
+
+        public EfCoreOrderRepository(ShopContext shopContext) : base(shopContext)
+        {
+            _dbContext = shopContext;
+        }
+
+        public Order GetMostExpensiveOrder()
+        {
+            //TODO:Write this method business
+            throw new NotImplementedException();
+        }
+    }
+}
