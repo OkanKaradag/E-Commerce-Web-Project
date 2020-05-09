21692bf654cb92c3594def2af1556908988dff7c
Okan Karadağ <okan.karadag@d-teknoloji.com.tr>
2020-05-10 02:07:04 +0300

    Add product service interface

---
+++ ShopApp/ShopApp.Business/Abstract/Services/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using ShopApp.Entities;
+using System.Collections.Generic;
+
+namespace ShopApp.Business.Abstract.Services
+{
+    public interface IProductService : IService<Product>
+    {
+        IEnumerable<Product> GetPopularProducts();
+    }
+}
