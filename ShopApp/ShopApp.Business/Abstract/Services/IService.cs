79484180aa6e67c2d1c86bcd115b0ce9e587b536
Okan Karadağ <okan.karadag@d-teknoloji.com.tr>
2020-05-10 02:06:10 +0300

    Add service interface

---
+++ ShopApp/ShopApp.Business/Abstract/Services/IService.cs
@@ -0,0 +1,22 @@
+﻿using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ShopApp.Business.Abstract
+{
+    public interface IService<TEntity>
+    {
+        TEntity GetById(int id);
+        TEntity GetByFilter(Expression<Func<TEntity, bool>> filter);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> GetListByFilter(Expression<Func<TEntity, bool>> filter);
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+        void Update(TEntity entity);
+        void UpdateRange(IEnumerable<TEntity> entities);
+        void Delete(TEntity entity);
+        void DeleteRange(IEnumerable<TEntity> entities);
+    }
+}
