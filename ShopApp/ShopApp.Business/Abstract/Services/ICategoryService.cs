eb2fdda1ea819289fc2fe7011da9bcf57d0cedb8
Okan <o.karadag@outlook.com>
2020-05-17 04:13:54 +0300

    Add category service interface

---
+++ ShopApp/ShopApp.Business/Abstract/Services/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using ShopApp.Entities;
+using System.Collections.Generic;
+
+namespace ShopApp.Business.Abstract.Services
+{
+    public interface ICategoryService : IService<Category>
+    {
+        IEnumerable<Category> GetSubCategories(int categoryId);
+    }
+}
