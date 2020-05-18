d5aa5de1b5e31f632c5d9d9b541fc8eba56a9ec0
Okan <o.karadag@outlook.com>
2020-05-18 04:20:19 +0300

    Add DbContext injection

--- ShopApp/ShopApp.WebUI/Startup.cs
+++ ShopApp/ShopApp.WebUI/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ShopApp.DataAccess.Concrete;
 
 namespace ShopApp.WebUI
 {
@@ -12,6 +13,9 @@ namespace ShopApp.WebUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            #region DbContext Injection
+            services.AddDbContext<ShopContext>();
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
